c2da004887b9ac5f83447519e047ca16ef966de9
Daniel Pereira <DanielPereira.programador@gmail.com>
2020-06-15 14:56:56 -0300

    construtores

---
+++ src/objetos/Pessoa.kt
@@ -0,0 +1,11 @@
+package objetos
+
+
+
+ class Pessoa (var nome: String)
+
+fun main(){
+  var x: Pessoa=Pessoa("Danel")
+    println(  x.nome  )
+
+}
---
+++ src/objetos/objeto01.kt
@@ -0,0 +1,6 @@
+package objetos
+
+fun main()
+{
+
+}
