d4326b47786f0820135cf79eee155d4d2991c36e
Daniel Pereira <DanielPereira.programador@gmail.com>
2020-06-15 15:25:12 -0300

    dois construtores

--- src/objetos/Pessoa.kt
+++ src/objetos/Pessoa.kt
@@ -1,11 +1,15 @@
-package objetos
+    package objetos
 
 
+    class Pessoa(var nome: String) {
+        constructor(idade: Int, nome: String) :this(nome) {
 
- class Pessoa (var nome: String)
 
-fun main(){
-  var x: Pessoa=Pessoa("Danel")
-    println(  x.nome  )
+        }
+    }
 
-}
+    fun main() {
+        var x: Pessoa = Pessoa(10, "Daniel")
+        println(x.nome)
+
+    }
